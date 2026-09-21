62334b5cc2bb9bba4f0e04bff6839632b6adacf2
TOMAHOK <tomahok5099+github@gmail.com>
2026-09-22 07:23:13 +0900

    　

--- docs/script.js
+++ docs/script.js
@@ -2,10 +2,12 @@ const CurrentTime=new Date().getHours();
 const Greetings=document.querySelector(".Greetings");
 if(CurrentTime>=20||CurrentTime<5){
     Greetings.textContent="Good Evening";
-}else if(CurrentTime>=15) {
+}else if(CurrentTime>=15){
     Greetings.textContent="Good Afternoon";
-}else if(CurrentTime>=10) {
+}else if(CurrentTime>=10){
     Greetings.textContent="Hello";
-}else{
+}else if(CurrentTime>=5){
     Greetings.textContent="Good Morning";
+}else{
+	Greetimgs.textContent="What time is it now?";
 }
