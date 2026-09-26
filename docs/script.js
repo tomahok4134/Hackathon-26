205924d26ded35e18b4d49015feccdb27291a107
manmen2414 <inotitokokoro@gmail.com>
2026-09-26 12:21:36 +0900

    fix: iframe内なら遷移

--- docs/script.js
+++ docs/script.js
@@ -12,8 +12,7 @@ if (CurrentTime >= 20 || CurrentTime < 5) {
   Greetings.textContent = "What time is it now??";
 }
 
-alert(new URL(location).searchParams.get("state"));
-if (new URL(location).searchParams.get("state") === "open") {
+if (window.self !== window.top) {
   globalLoginData.then(([login]) => {
     if (login) location.href = getURL(`home/index.html`);
     else location.href = getURL(`index.html`);
